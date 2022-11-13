7a6cb5251abec67609c72655b800e3edcbd988df
Stas Vasiliev <uperman5@yandex.ru>
2022-11-13 03:57:47 +0300

    .token_blacklist added

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
-    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'api.apps.ApiConfig',
     'reviews.apps.ReviewsConfig',
 ]
