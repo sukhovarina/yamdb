1629a2df3a01ba0bdf73642acaa8ce824a9b1445
Stas Vasiliev <uperman5@yandex.ru>
2022-11-13 03:53:16 +0300

    вернул все назад

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
-    # 'rest_framework_simplejwt',
+    'rest_framework_simplejwt',
     'api.apps.ApiConfig',
     'reviews.apps.ReviewsConfig',
 ]
